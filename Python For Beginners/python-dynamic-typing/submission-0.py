801237eabea9a334edac1d38251533b3d7f9d15d
cyberboynj <njmv8299@gmail.com>
2026-09-14 13:21:39 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 8
+print(type(variable))
+variable = 8.2
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Hi there"
+print(type(variable))
+variable = [2, 4, 6]
+print(type(variable))
