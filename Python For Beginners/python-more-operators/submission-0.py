77b3137b3a0454ac3d5db6710f906ba9d9d5279f
cyberboynj <njmv8299@gmail.com>
2026-09-14 14:32:32 -0700

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,14 @@
+a, b, c = 2, 8, 5 
+
+floorProd = (a * b) // c
+print(floorProd)
+
+remainderProd = (a * b) % c
+print(remainderProd)
+
+powerB = a ** b
+print(powerB)
+
+powerC = b ** c 
+print(powerC)
+
