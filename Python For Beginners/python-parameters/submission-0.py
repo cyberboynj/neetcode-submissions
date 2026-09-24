5c5b130f3ba73d68470bdd21c7cbc9a5a62c605e
cyberboynj <njmv8299@gmail.com>
2026-09-23 17:40:50 -0700

    Add: python-parameters - submission-0

---
+++ Python For Beginners/python-parameters/submission-0.py
@@ -0,0 +1,8 @@
+def farewell(communication):
+    gesture = "Goodbye, " + communication
+    print(gesture)
+
+# don't modify below this line
+farewell("Bob")
+farewell("Charlie")
+farewell("NeetCode")
